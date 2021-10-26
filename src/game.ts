3a0f075f4632fe532d23894ae3e1c07e4975755c
Hiroshi Urayama <urahiroshi@mercari.com>
2021-10-26 18:19:42 +0900

    change to shooting game

--- src/game.ts
+++ src/game.ts
@@ -1,31 +1,37 @@
 import * as Phaser from "phaser"
 
 export class Game extends Phaser.Scene {
-  private ball: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-  private paddle: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private balls: Phaser.Types.Physics.Arcade.Group;
+  private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
   public init() { }
 
   public preload() {
     this.load.setBaseURL('../assets');
     this.load.image('ball', 'ball.png');
-    this.load.image('paddle', 'paddle.png');
+    this.load.image('square', 'square.png');
   }
 
   public create() {
     const { width, height } = this.sys.game.canvas;
 
-    this.ball = this.physics.add.sprite(width * 0.5, height - 25, 'ball');
-    this.ball.setVelocity(150, -150);
-    this.ball.setCollideWorldBounds(true);
-    this.ball.setBounce(1);
+    this.balls = this.physics.add.group({
+      key: 'ball',
+      frameQuantity: 10,
+      velocityX: 0,
+      velocityY: 100,
+    });
+    Phaser.Actions.RandomRectangle(this.balls.getChildren(), new Phaser.Geom.Rectangle(0, 0, width, 0));
 
-    this.paddle = this.physics.add.sprite(width * 0.5, height - 5, 'paddle');
-    this.paddle.body.immovable = true;
+    this.player = this.physics.add.sprite(width * 0.5, height - 10, 'square');
+    this.player.body.immovable = true;
   }
 
   public update() {
-    this.physics.collide(this.ball, this.paddle);
-    this.paddle.x = this.game.input.mousePointer.x || this.sys.game.canvas.width * 0.5;
+    this.player.x = this.game.input.mousePointer.x || this.sys.game.canvas.width * 0.5;
+    if (this.physics.collide(this.balls, this.player)) {
+      alert('game over');
+      location.reload();
+    }
   }
 }

ac2f60fb0170a4675d2e5561407c8d081d47772e
Hiroshi Urayama <urahiroshi@mercari.com>
2021-10-26 15:21:00 +0900

    add paddle

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,31 @@
 import * as Phaser from "phaser"
 
 export class Game extends Phaser.Scene {
+  private ball: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private paddle: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
   public init() { }
 
   public preload() {
-    this.load.image('ball', '../assets/ball.png');
+    this.load.setBaseURL('../assets');
+    this.load.image('ball', 'ball.png');
+    this.load.image('paddle', 'paddle.png');
   }
 
   public create() {
-    const ball = this.physics.add.sprite(50, 50, 'ball');
-    ball.setVelocity(100, 100);
-    ball.setCollideWorldBounds(true);
-    ball.setBounce(1);
+    const { width, height } = this.sys.game.canvas;
+
+    this.ball = this.physics.add.sprite(width * 0.5, height - 25, 'ball');
+    this.ball.setVelocity(150, -150);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
+
+    this.paddle = this.physics.add.sprite(width * 0.5, height - 5, 'paddle');
+    this.paddle.body.immovable = true;
+  }
+
+  public update() {
+    this.physics.collide(this.ball, this.paddle);
+    this.paddle.x = this.game.input.mousePointer.x || this.sys.game.canvas.width * 0.5;
   }
 }
